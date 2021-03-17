347965fee41dbe625f01f8edd40767c769910dff
RickYoon <yoon1515@gmail.com>
2021-03-17 20:32:20 +0900

    1st page end

--- pages/index.js
+++ pages/index.js
@@ -3,6 +3,7 @@ import Head from "next/head";
 import Navbar from "./src/component/Navbar";
 import Link from "next/link";
 import axios from "axios";
+import ReactLoading from "react-loading";
 
 export default function Home() {
   const [isLoading, setIsLoading] = useState(false);
@@ -50,17 +51,8 @@ export default function Home() {
       // url: `https://z5v2zc0s9i.execute-api.ap-northeast-2.amazonaws.com/production/getLifeInfo`,
     }).then((res) => {
       setIsLoading(false);
-      // console.log(res.data.Items);
-      // setEvents(res.data.Items);
-      // console.log(events);
       var sortingField = "eventDate";
       console.log(res.data.Items);
-      // console.log(
-      // res.data.Items.sort(function (a, b) {
-      //   return b.sortingField - a.sortingField;
-      // });
-      // console.log(res.data.Items[2].eventDate);
-
       if (searchtype === "fromtoday") {
         res.data.Items.sort(function (a, b) {
           if (a.eventDate < b.eventDate) {
@@ -92,7 +84,7 @@ export default function Home() {
 
   const searchbutton = (e) => {
     // window.location.href = "/add";
-    console.log(e);
+    // console.log(e);
   };
   function searchstart(event) {
     // console.log(event);
@@ -116,210 +108,212 @@ export default function Home() {
     console.log("click button");
   };
 
-  const loadUserstwo = async () => {
-    const result = await axios({
-      method: "GET",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      url: `https://ddjw33n2b0.execute-api.ap-northeast-2.amazonaws.com/production/queryEvents?searchtype=${searchtype}&keyword=${keyword}`,
-      // url: `https://z5v2zc0s9i.execute-api.ap-northeast-2.amazonaws.com/production/getLifeInfo`,
-    }).then((res) => {
-      // console.log(res.data.Items);
-      // setEvents(res.data.Items);
-      // console.log(events);
-      var sortingField = "eventDate";
-      console.log(res.data.Items);
-      // console.log(
-      // res.data.Items.sort(function (a, b) {
-      //   return b.sortingField - a.sortingField;
-      // });
-      // console.log(res.data.Items[2].eventDate);
-
-      // console.log(res.data.Items[2].eventDate);
-
-      // // );
-      // console.log(res.data.Items);
-
-      setEvents(res.data.Items);
-      // console.log(events);
-    });
-  };
-
-  <style jsx global>{`
-    html,
-    body {
-      padding: 0;
-      margin: 0;
-      font-family: Lobster;
-    }
-
-    * {
-      box-sizing: border-box;
-    }
-  `}</style>;
-
-  // console.log(process.env.NEXT_PUBLIC_API_URL);
   return (
-    <div style={{ backgroundColor: "#f1efe4" }}>
-      <Navbar />
-      <div className="container mx-auto desktop mb-2 md:mb-10">
-        <div className="w-full md:w-5/6 mx-auto bg-white p-4 rounded-md shadow-lg mt-5">
-          <form onSubmit>
-            <p className="text-left text-xl ff font-semibold mb-1">
-              관심있는 이벤트 찾기
-            </p>{" "}
-            <div className="w-full">
-              <input
-                style={{ fontSize: "12px" }}
-                className="bg-purple-white bg-gray-200 shadow rounded border-0 p-3 w-75 mt-2"
-                placeholder="검색어를 입력하고 검색을 누르세요"
-              />
-              <button className="bg-gray-500 rounded border-0 w-25 p-3">
-                검색
-              </button>
-            </div>
-          </form>{" "}
-        </div>
-        {searchtype === "fromtoday" ? (
-          <div
-            className="w-full mt-3 rounded-md"
-            style={{
-              display: "flex",
-              alignItems: "center",
-              justifyContent: "center",
-              backgroundColor: "white",
-              textAlign: "center",
-              borderRadius: "100px",
-            }}
-          >
-            <div
-              className="card-body w-50"
-              style={{ backgroundColor: "white" }}
-              onClick={() => {
-                console.log("fromtoday");
-                setSearchtype("fromtoday");
-              }}
-            >
-              from today
-            </div>
-            <div
-              className="card-body w-50"
-              style={{ backgroundColor: "gray" }}
-              onClick={() => {
-                console.log("newupdates");
-                setSearchtype("newupdates");
-              }}
-            >
-              new updates
-            </div>
+    <>
+      <Head>
+        {" "}
+        <meta name="title" content="goClubHouse" />
+        <meta
+          name="description"
+          content="goclubhouse 를 통해서 더 많은 이벤트를 만나보세요."
+        />
+        <meta
+          name="keywords"
+          content="클럽하우스, RSVP, clubhouse, goclubhouse, 스타트업, 재테크, 주식, 명상, 인싸, 클하초대장, 클하이벤트"
+        />
+        <meta property="og:type" content="website" />
+        <meta
+          property="og:title"
+          content="goClubHouse - 클럽하우스를 통해 더 넓은 세상을 만나는 방법"
+        />
+        <meta
+          property="og:description"
+          content="클럽하우스를 통해서 더 많은 이벤트와 사람을 만날 수 있도록 돕습니다."
+        />
+        <meta property="og:site_name" content="goClubHouse" />
+        <meta name="twitter:card" content="summary" />
+        <meta
+          name="twitter:title"
+          content="쇼피랩스 shopeelabs - 마진계산기 & 시장조사"
+        />
+        <meta
+          name="twitter:description"
+          content="쇼피셀러를 위한 모든 툴과 정보를 제공하기 위해서 연구하는 쇼피랩스입니다. 쇼피제품판매를 위해 소요되는 시간을 절약하고, 더 잘팔릴 물건을 올릴 수 있도록 돕도록 하겠습니다."
+        />{" "}
+        <link
+          href="https://fonts.googleapis.com/css2?family=Noto+Sans+KR:wght@700&display=swap"
+          rel="stylesheet"
+        />
+      </Head>
+      <div style={{ backgroundColor: "#f1efe4" }}>
+        <Navbar />
+        <div className="container mx-auto desktop mb-2 md:mb-10">
+          <div className="w-full md:w-5/6 mx-auto bg-white p-4 rounded-md shadow-lg mt-5">
+            <form onSubmit={searchstart}>
+              <p className="text-left text-xl ff font-semibold mb-1">
+                관심있는 이벤트 찾기
+              </p>{" "}
+              <div className="w-full">
+                <input
+                  onChange={onChange}
+                  value={keyword}
+                  style={{ fontSize: "12px" }}
+                  className="bg-purple-white bg-gray-200 shadow rounded border-0 p-3 w-75 mt-2"
+                  placeholder="검색어를 입력하고 검색을 누르세요"
+                />
+                <button
+                  className="bg-gray-500 rounded border-0 w-25 p-3"
+                  onClick={onReset}
+                >
+                  검색
+                </button>
+              </div>
+            </form>{" "}
           </div>
-        ) : (
-          <div
-            className="w-full mt-3 rounded-md"
-            style={{
-              display: "flex",
-              alignItems: "center",
-              justifyContent: "center",
-              backgroundColor: "white",
-              textAlign: "center",
-            }}
-          >
+          {searchtype === "fromtoday" ? (
             <div
-              className="card-body w-50"
-              style={{ backgroundColor: "gray" }}
-              onClick={() => {
-                console.log("fromtoday");
-                setSearchtype("fromtoday");
-                // loadUsers(searchtype);
+              className="w-full mt-3 rounded-md"
+              style={{
+                display: "flex",
+                alignItems: "center",
+                justifyContent: "center",
+                backgroundColor: "white",
+                textAlign: "center",
+                borderRadius: "100px",
               }}
             >
-              from today
-            </div>
-            <div
-              className="card-body w-50"
-              style={{ backgroundColor: "white" }}
-              onClick={() => {
-                console.log("newupdates");
-                setSearchtype("newupdates");
-
-                // loadUsers(searchtype);
-              }}
-            >
-              new updates
-            </div>
-          </div>
-        )}
-        {events.map((event) => (
-          <Link
-            href={`/event/[id]`}
-            as={`/event/${event.eventCode}`}
-            style={{
-              textDecoration: "none",
-              color: "black",
-            }}
-          >
-            <div
-              className="w-full mt-3 rounded-md shadow-lg "
-              onClick={() => {
-                console.log(event.eventCode);
-              }}
-            >
-              <div className="card rounded-md">
-                <div className="card-body rounded-md">
-                  <p className="card-subtext" style={{ fontSize: "12px" }}>
-                    {event.datetimeKorea}
-                  </p>
-                  <h5 className="card-title" style={{ fontSize: "15px" }}>
-                    {event.title}
-                  </h5>
-                  {event.images.map((kk, index) => (
-                    <div className="avatarbox">
-                      <div className="innerbox">
-                        <img
-                          src={kk}
-                          alt="Avatar"
-                          width="40px"
-                          style={{ borderRadius: "50%", marginLeft: "5px" }}
-                        ></img>
-                        <div
-                          style={{
-                            fontSize: "10px",
-                            width: "50px",
-                            height: "30px",
-                          }}
-                        >
-                          {event.moderators[index]}
-                        </div>
-                      </div>
-                    </div>
-                  ))}
-                  <p className="card-subtext mt-3" style={{ fontSize: "12px" }}>
-                    {event.description}
-                    {name} 환경입니다.
-                  </p>
-                </div>
+              <div
+                className="card-body w-50"
+                style={{ backgroundColor: "white" }}
+                onClick={() => {
+                  console.log("fromtoday");
+                  setSearchtype("fromtoday");
+                }}
+              >
+                from today
+              </div>
+              <div
+                className="card-body w-50"
+                style={{ backgroundColor: "gray" }}
+                onClick={() => {
+                  console.log("newupdates");
+                  setSearchtype("newupdates");
+                }}
+              >
+                new updates
               </div>
             </div>
-          </Link>
-        ))}
+          ) : (
+            <div
+              className="w-full mt-3 rounded-md"
+              style={{
+                display: "flex",
+                alignItems: "center",
+                justifyContent: "center",
+                backgroundColor: "white",
+                textAlign: "center",
+              }}
+            >
+              <div
+                className="card-body w-50"
+                style={{ backgroundColor: "gray" }}
+                onClick={() => {
+                  console.log("fromtoday");
+                  setSearchtype("fromtoday");
+                  // loadUsers(searchtype);
+                }}
+              >
+                from today
+              </div>
+              <div
+                className="card-body w-50"
+                style={{ backgroundColor: "white" }}
+                onClick={() => {
+                  console.log("newupdates");
+                  setSearchtype("newupdates");
+
+                  // loadUsers(searchtype);
+                }}
+              >
+                new updates
+              </div>
+            </div>
+          )}
+          {isLoading ? (
+            <ReactLoading
+              type="cubes"
+              color="gray"
+              style={{
+                textAlign: "center",
+                width: "150px",
+                margin: "auto",
+                height: "500px",
+              }}
+            />
+          ) : (
+            <div></div>
+          )}
+          {events.map((event) => (
+            <Link
+              href={`/event/[id]`}
+              as={`/event/${event.eventCode}`}
+              style={{
+                textDecoration: "none",
+                color: "black",
+              }}
+            >
+              <div
+                className="w-full mt-3 rounded-md shadow-lg "
+                onClick={() => {
+                  console.log(event.eventCode);
+                }}
+              >
+                <div
+                  className="card rounded-md"
+                  style={{ borderRadius: "12px" }}
+                >
+                  <div className="card-body rounded-md">
+                    <p className="card-subtext" style={{ fontSize: "12px" }}>
+                      {event.datetimeKorea}
+                    </p>
+                    <h5 className="card-title" style={{ fontSize: "15px" }}>
+                      {event.title}
+                    </h5>
+                    {event.images.map((kk, index) => (
+                      <div className="avatarbox">
+                        <div className="innerbox">
+                          <img
+                            src={kk}
+                            alt="Avatar"
+                            width="40px"
+                            style={{ borderRadius: "50%", marginLeft: "5px" }}
+                          ></img>
+                          <div
+                            style={{
+                              fontSize: "10px",
+                              width: "50px",
+                              height: "30px",
+                            }}
+                          >
+                            {event.moderators[index]}
+                          </div>
+                        </div>
+                      </div>
+                    ))}
+                    <p
+                      className="card-subtext mt-3"
+                      style={{ fontSize: "12px" }}
+                    >
+                      {event.description}
+                      {name} 환경입니다.
+                    </p>
+                  </div>
+                </div>
+              </div>
+            </Link>
+          ))}
+        </div>
       </div>
-    </div>
+    </>
   );
 }
-
-// 여기는 브라우저 환경이 아님
-// export async function getStaticProps() {
-//   const apiUrl =
-//     "https://ddjw33n2b0.execute-api.ap-northeast-2.amazonaws.com/production/queryEvents?searchtype=newupdates&keyword=";
-//   const res = await Axios.get(apiUrl);
-//   const data = res.data;
-//   // const kk = [1, 2, 3, 4];
-
-//   return {
-//     props: {
-//       events: data.Items,
-//       name: process.env.name,
-//     },
-//   };
-// }
--- pages/src/component/Navbar.js
+++ pages/src/component/Navbar.js
@@ -4,7 +4,11 @@ export default function Navbar() {
   return (
     <nav className="navbar navbar-expand-lg navbar-dark bg-danger fixed-top">
       <div className="container">
-        <a className="navbar-brand" href="/" style={{ float: "left" }}>
+        <a
+          className="navbar-brand"
+          href="/"
+          style={{ float: "left", fontWeight: "bolder" }}
+        >
           goClubHouse
         </a>
         <Link className="navbar-brand" href="/add">
